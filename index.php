33ea14e8fd7b45cfbb067b80d91c410605f1fb32
PavleNeskovic <pavlenesko@gmail.com>
2018-05-12 14:57:01 +0200

    Bootstrap added (DZ05)

--- index.php
+++ index.php
@@ -4,45 +4,47 @@
   <title>IT255</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="style/style.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
 
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-</div>
+<nav class="navbar navbar-inverse topnav">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#home">WebSiteName</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#home">Home</a></li>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </ul>
+  </div>
+</nav>
 
 <div class="container">
   <div class="jumbotron">
     <h1 class="headline">What is Lorem Ipsum?</h1> 
-    <button type="button" class="time-button" onclick="document.getElementById('time').innerHTML = Date()">
-      Display Date and Time</button>
-    <p id="time" class="subtitle"></p>
     <p class="subtitle">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+  </div>
 
+  <div class="col-sm-12">
     
   </div>
 
-  <div class="content">
-    <div class="subtext">
-      <h1>Why do we use it?</h1> 
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type
-         specimen book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. It was popularised 
-          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-           passages, and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.</p> 
-    </div>
-  </div>
 
-  <div class="content">
-    <div class="subtext">
-      <h1>Why do we use it?</h1> 
+  <div class="row content">
+  <div class="col-sm-4 subtext">
+    <h1>What time is it?</h1> 
+    <button type="button" class="btn btn-default btn-lg " onclick="document.getElementById('time').innerHTML = Date()">
+      Display Date and Time</button>
+    <p id="time" class="subtitle"></p>
+    </div>
+    <div class="col-sm-4 subtext">
+    <h1>Why do we use it?</h1> 
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
         when an unknown printer took a galley of type and scrambled it to make a type
@@ -50,73 +52,63 @@
           electronic typesetting, remaining essentially unchanged. It was popularised 
           in the 1960s with the release of Letraset sheets containing Lorem Ipsum
            passages, and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.</p> 
+            PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+    <div class="col-sm-4 subtext">
+    <h1>Why do we use it?</h1> 
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type
+         specimen book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was popularised 
+          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+           passages, and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+    <div class="col-sm-4 subtext">
+    <h1>Why do we use it?</h1> 
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type
+         specimen book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was popularised 
+          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+           passages, and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+    <div class="col-sm-4 subtext">
+    <h1>Why do we use it?</h1> 
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type
+         specimen book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was popularised 
+          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+           passages, and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.</p>
+    </div>
+    <div class="col-sm-4 subtext">
+    <h1>Why do we use it?</h1> 
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+        when an unknown printer took a galley of type and scrambled it to make a type
+         specimen book. It has survived not only five centuries, but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was popularised 
+          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+           passages, and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.</p>
     </div>
   </div>
+</div>
 
-  <div class="content">
-    <div class="subtext">
-      <h1>Why do we use it?</h1> 
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type
-         specimen book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. It was popularised 
-          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-           passages, and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.</p> 
-    </div>
-  </div>
-
-    <div class="content">
-    <div class="subtext">
-      <h1>Why do we use it?</h1> 
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type
-         specimen book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. It was popularised 
-          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-           passages, and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.</p> 
-    </div>
-  </div>
-
-    <div class="content">
-    <div class="subtext">
-      <h1>Why do we use it?</h1> 
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type
-         specimen book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. It was popularised 
-          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-           passages, and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.</p> 
-    </div>
-  </div>
-
-    <div class="content">
-    <div class="subtext">
-      <h1>Why do we use it?</h1> 
-      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-        when an unknown printer took a galley of type and scrambled it to make a type
-         specimen book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. It was popularised 
-          in the 1960s with the release of Letraset sheets containing Lorem Ipsum
-           passages, and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.</p> 
-    </div>
-  </div>
 
 </div>
 
 
 
-<div class="footer">
+<footer class="footer">
   <p>Pavle Neskovic 1998</p>
-</div>
+</footer>
 
 <script>
 
